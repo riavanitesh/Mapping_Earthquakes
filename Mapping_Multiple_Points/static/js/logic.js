29752152478c2499b3f6f48a824e10dc038d3164
riavanitesh <96365651+riavanitesh@users.noreply.github.com>
2022-04-02 16:50:44 -0300

    Added lines

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -1,5 +1,7 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
+
+
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
@@ -13,43 +15,11 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
+// Get data from cities.js
+let cityData = cities;
 
 //  Add a marker to the map for Los Angeles, California.
 let cities = L.cities([34.0522, -118.2437]).addTo(map);
-
-
-// An array containing each city's location, state, and population.
-let cities = [{
-  location: [40.7128, -74.0059],
-  city: "New York City",
-  state: "NY",
-  population: 8398748
-},
-{
-  location: [41.8781, -87.6298],
-  city: "Chicago",
-  state: "IL",
-  population: 2705994
-},
-{
-  location: [29.7604, -95.3698],
-  city: "Houston",
-  state: "TX",
-  population: 2325502
-},
-{
-  location: [34.0522, -118.2437],
-  city: "Los Angeles",
-  state: "CA",
-  population: 3990456
-},
-{
-  location: [33.4484, -112.0740],
-  city: "Phoenix",
-  state: "AZ",
-  population: 1660272
-}
-];
 
 
 // Loop through the cities array and create one marker for each city.
@@ -57,4 +27,22 @@ cities.forEach(function(city) {
   console.log(city)
   L.marker(city.location).addTo(map);
 });
+
+// Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+  console.log(city)
+  L.marker(city.location).addTo(map);
+});
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+  console.log(city)
+  L.marker(city.location)
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
+.addTo(map);
+});
+
 
